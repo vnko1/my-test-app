1abc92af15d4343eda878873e4745686fe98a47d
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-22 23:49:17 +0300

    edit gh sets

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store, persistStor } from "./redux/index";
 import App from "./App.jsx";
 import UsersProvider from "./components/UsersContext";
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStor}>
         <UsersProvider>
-          <HashRouter>
+          <BrowserRouter basename="/my-test-app">
             <App />
-          </HashRouter>
+          </BrowserRouter>
         </UsersProvider>
       </PersistGate>
     </Provider>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-test-app/",
+  base: "/my-test-app",
+  server: {
+    open: "/my-test-app",
+  },
 });
